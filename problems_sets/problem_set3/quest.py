0b92262b37ce0b44ac37c6738fae2a00d2e1ba51
Biradater Adarkwah <rc.adarkwah.biradater@leadps.org>
2016-11-09 17:15:47 +0000

    Final problem sets edits

--- problems_sets/problem_set3/quest.py
+++ problems_sets/problem_set3/quest.py
@@ -1,13 +1,15 @@
+import random
+
 print("Welcome to Bree's Land Quest!")
 print("Enter the name of your character: ")
 name = raw_input()
 print("You will now be asked to enter you strength, health and luck levels. It is preferred to enter a total amount of 15 so choose wisely if you do!")
 print("Enter strength (1-10): ")
-strength = raw_input()
+strength = int(raw_input())
 print("Enter health (1-10): ")
-health = raw_input()
+health = int(raw_input())
 print("Enter luck (1-10): ")
-luck = raw_input()
+luck = int(raw_input())
 
 if int(strength) + int(health) + int(luck) <= 15:
 	print("Hi " + str(name) + "! Your strength is " + str(strength) + ". Your health is " + str(health) + ". Your luck is " + str(luck) + ".")
@@ -16,6 +18,7 @@ if int(strength) + int(health) + int(luck) > 15:
 	strength = 5
 	health = 5
 	luck = 5
+realLuck = random.randint(0,10)
 
 print("Now " + name + ", you've come to a fork in the road. Do you want to go right or left? Enter right or left")
 choice = raw_input()
@@ -26,7 +29,7 @@ if (choice == 'left') or (choice == 'Left'):
 		print("Due to your high strength, you were able to fight and kill all the dragons and alligators! You won!")
 	elif health >= 7:
 		print("Due to your high health level, although you did get bitten by a few alligators and got hit with some dragons' fire while swimming across the lake, you were able to heal and recover quickly! You won!")
-	elif luck >= 7:
+	elif luck >= realLuck:
 		print("Due to your high luck, a fairy magically appeared and was able to transport you across the lake without any harm done! You won!")
 	elif (strength) and (health) and (luck) < 7:
 		print("Sorry, your strength, your health, nor your luck could save you from this situation. You were eaten by alligators and caught on fire by the dragons. You lost!")
@@ -37,7 +40,7 @@ if (choice == 'right') or (choice == 'Right'):
                 print("Due to your high strength, you were able to fight and kill all the wolves and bats! You won!")
         elif health >= 7:
                 print("Due to your high health level, although you did get bitten by a few wolves and got hit with a few bats while running through the forest, you were able to recover quickly. You won!")
-        elif luck >= 7:
+        elif luck >= realLuck:
                 print("Due to your high luck, the wolves and bats ignored you as if you were invisible, You were able to get through the forest without any mark left on you! You won!")
         elif (strength) and (health) and (luck) < 7:
-                print("Sorry, your strength, your health, nor your luck could save you from this situation. You were eaten by the pack of wolves and the bats decomposed your body.")
+                print("Sorry, your strength, your health, nor your luck could save you from this situation. You were eaten by the pack of wolves and the bats drank your blood. You lost.")
