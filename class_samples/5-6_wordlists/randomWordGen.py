4e3c820535e1f1cff857b689133d07b8cd63e127
Biradater Adarkwah <rc.adarkwah.biradater@leadps.org>
2017-03-27 17:10:18 +0000

    Comments

--- class_samples/5-6_wordlists/randomWordGen.py
+++ class_samples/5-6_wordlists/randomWordGen.py
@@ -1,7 +1,9 @@
 # Produces a different random word every time
 
+# Allows the program to use the random function
 import random
 
+# Opens the words.txt file and adds each line to the list
 wordsFile = open('words.txt', 'r')
 wordsList = []
 
@@ -11,7 +13,9 @@ while myWord != '':
         wordsList.append(myWord)
         myWord = wordsFile.readline()
 
+# This creates a variable to a random word in the list
 randomWord = random.choice(wordsList)
 
+#This prints the random word
 print("Here is a random word:")
 print(randomWord)
